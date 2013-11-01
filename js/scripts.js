040abe7f9f453e36fa2636add181c95ad60df177
Ashley Chaffin <graphicdesign@graphics-imac-9.clients.nic.edu>
2013-11-01 14:13:42 -0700

    Learning Transit Plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-// Scripts
+$(document).ready(function(){
+    
+    $('h1').transition({
+        x: '100px',
+        y: '100px'
+    
+    });
+    
+    $('img').transition({ x: 100 })
+  .transition({ y: 100 })
+  .transition({ x: 200 })
+  .transition({ y: 500 });
+
+    
+}); //document.ready end
